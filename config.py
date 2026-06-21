044e3fe8b15e1e3e59103c78922ea1fcc74cdfe1
Emriqurrizal Yahya Nurramdahan <emriqurrizal@gmail.com>
2026-06-21 16:30:08 +0700

    fix: removing hardcoded credentials

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 NEO4J_URI      = os.getenv("NEO4J_URI", "neo4j://127.0.0.1:7687")
 NEO4J_USER     = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "emriq123")   
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 DATA_DIR = r"C:\Users\Emriqurrizal\Downloads\symtomps"
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
